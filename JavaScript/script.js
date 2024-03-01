2b0c500acd682f69b30602292eff030e1f9f8adf
Kavippiriyan <kavippiriyan2001@gmail.com>
2024-03-01 12:13:10 +0530

    new foler added

---
+++ JavaScript/script.js
@@ -0,0 +1,72 @@
+//    Variables
+
+
+/* 
+1. var
+2. let 
+3. const
+*/
+
+// var
+/*
+var a = 10;
+var b = 20;
+var c = a+b;
+console.log(c);
+var a = 100;
+var c = a+b;
+console.log(c);
+
+var name = "apple";
+console.log(name);
+{
+    var name = "Mango";
+}
+
+console.log(name);
+
+*/
+
+// let 
+
+/*
+let a = 10;
+let b = 20;
+let c = a+b;
+console.log(c);
+
+let name = "Elephant";
+
+{
+    let name = "Eagle";
+    console.log(name);
+}
+console.log(name);
+
+*/
+
+
+// const a =10;
+// const b = 20;
+// const c = a+b;
+// console.log(c);
+
+// const name = "lion"
+
+// {
+//     const name  ="tiger"
+// }
+
+// console.log(name);
+// const a =10;
+
+// {
+//     const a=20;
+// }
+// console.log(a);
+
+
+
+num = prompt("please enter the number");
+
+console.log(num-2);
